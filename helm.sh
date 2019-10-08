c6b7b61b54ef376cb217a8b12ece870056b2f129
ralavay <tungns.inf@gmail.com>
2019-10-08 10:29:27 +0800

    Quote var

--- helm.sh
+++ helm.sh
@@ -4,4 +4,4 @@ CMD="helm"
 SAFE_OPS="completion diff fetch get history ls list lint test status version"
 
 DIR=$(dirname "$0")
-source $DIR/confirm_k8s.sh
+source "$DIR"/confirm_k8s.sh
--- kubectl.sh
+++ kubectl.sh
@@ -4,4 +4,4 @@ CMD="kubectl"
 SAFE_OPS="get describe version"
 
 DIR=$(dirname "$0")
-source $DIR/confirm_k8s.sh
+source "$DIR"/confirm_k8s.sh
