3af7ebff4d6bf3b67140dc22201d6a06ef56fd06
ralavay <tungns.inf@gmail.com>
2019-10-08 22:11:34 +0800

    Wrong syntax

--- helm.sh
+++ helm.sh
@@ -5,4 +5,4 @@ SAFE_OPS="completion diff fetch get history ls list lint test status version"
 
 DIR=$(dirname "$0")
 # shellcheck source=confirm_k8s.sh
-source "$(DIR)/confirm_k8s.sh"
+source "${DIR}/confirm_k8s.sh"
--- kubectl.sh
+++ kubectl.sh
@@ -4,5 +4,6 @@ CMD="kubectl"
 SAFE_OPS="get describe version"
 
 DIR=$(dirname "$0")
+
 # shellcheck source=confirm_k8s.sh
-source "$(DIR)/confirm_k8s.sh"
+source "${DIR}/confirm_k8s.sh"
