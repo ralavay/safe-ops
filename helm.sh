8b690d13ee7c2f5d1efaa88b9d043307059ee086
ralavay <tungns.inf@gmail.com>
2019-10-08 10:20:39 +0800

    Load current dir of the script

--- helm.sh
+++ helm.sh
@@ -3,4 +3,5 @@
 CMD="helm"
 SAFE_OPS="completion diff fetch get history ls list lint test status version"
 
-source ./confirm_k8s.sh
+DIR=$(dirname "$0")
+source $DIR/confirm_k8s.sh
--- kubectl.sh
+++ kubectl.sh
@@ -3,4 +3,5 @@
 CMD="kubectl"
 SAFE_OPS="get describe version"
 
-source ./confirm_k8s.sh
+DIR=$(dirname "$0")
+source $DIR/confirm_k8s.sh
